66dfe215d114696740744b167b5ed6e0dfd7d220
rouzwelt <rouzweltt@gmail.com>
2024-05-24 16:14:00 +0000

    update

--- test-contracts/erc165/ERC165Supported.sol
+++ test-contracts/erc165/ERC165Supported.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CAL
-pragma solidity >=0.6.0;
+pragma solidity =0.8.25;
 
 import {IERC165} from "../../lib/forge-std/src/interfaces/IERC165.sol";
 
--- test-contracts/erc165/NonERC165.sol
+++ test-contracts/erc165/NonERC165.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CAL
-pragma solidity >=0.6.0;
+pragma solidity =0.8.25;
 
 /// A contract that doesnt implement erc165
 contract NonERC165 {
